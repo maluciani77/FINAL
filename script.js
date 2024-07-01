bc11ccddc64d4ac12dd02ae0df14c09f7dfcc2aa
maluciani77 <164639568+maluciani77@users.noreply.github.com>
2024-07-01 18:46:13 -0300

    script.js

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
 
-document.getElementById('toggle').addEventListener('click', function() {
-    document.body.classList.toggle('dark-mode');
-
-    if (document.body.classList.contains('dark-mode')) {
-        this.textContent = 'Modo Claro';
-    } else {
-        this.textContent = 'Modo Oscuro';
-    }
-});
-
 const toggle = document.getElementById('toggle');
 const body = document.querySelector('body');
 
